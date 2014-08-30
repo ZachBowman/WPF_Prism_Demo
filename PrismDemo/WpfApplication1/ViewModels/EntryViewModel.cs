71e898d366195489a3582e15eee3702b9d6bf493
Zach <zachbowman0@gmail.com>
2014-08-30 11:19:14 -0700

    first commit

---
+++ PrismDemo/WpfApplication1/ViewModels/EntryViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WpfApplication1.ViewModels
+{
+    public class EntryViewModel : NotificationObject
+    {
+        public DelegateCommand<string> SaveEntryCommand { get; private set; }
+
+        public ObservableCollection<string> Entries { get; private set; }
+        
+        private string currentText;
+        public string CurrentText 
+        {
+            get
+            {
+                return this.currentText;
+            }
+            set
+            {
+                if (string.Compare(value, currentText) != 0)
+                {
+                    this.currentText = value;
+                    this.RaisePropertyChanged(() => this.CurrentText);
+                }
+            }
+        }
+        
+        public EntryViewModel()
+        {
+            this.SaveEntryCommand = new DelegateCommand<string>((x) => this.PerformSaveEntry(x), (x) => this.CanSaveEntry(x));
+            this.Entries = new ObservableCollection<string>();
+        }
+
+        private bool CanSaveEntry(string newText)
+        {
+            return true;
+        }
+
+        public void PerformSaveEntry(string newText)
+        {
+            this.Entries.Add(newText);
+            ////Console.WriteLine("Add New");
+        }
+    }
+}
